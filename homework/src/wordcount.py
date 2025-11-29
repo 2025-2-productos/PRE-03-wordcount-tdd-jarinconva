d6c5a38f68c99265531237289e41800e519c7952
jarinconva <jarinconva@unal.edu.co>
2025-11-29 12:46:10 -0500

    creacion de test en test_internals

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -24,6 +24,26 @@ def parse_args():
     return parsed_args.input, parsed_args.output
 
 
+def preprocess_lines(lines):
+    pass
+
+
+def split_into_words(preprocessed_lines):
+    pass
+
+
+def count_words(words):
+    pass
+
+
+def write_word_count(output_folder, word_count):
+    pass
+
+
 def main():
     input_folder, output_folder = parse_args()
     lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_count = count_words(words)
+    write_word_count(output_folder, word_count)
