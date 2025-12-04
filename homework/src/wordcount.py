bd968ee0b8dc8687cf8b852cca8577a38586d71d
jarinconva <jarinconva@unal.edu.co>
2025-12-04 13:50:58 -0500

    Creacion de test en test_internals.py

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -3,7 +3,6 @@
 
 
 import argparse
-import sys
 
 from homework.src._internals.read_all_lines import read_all_lines
 
@@ -36,7 +35,7 @@ def count_words(words):
     pass
 
 
-def write_word_count(output_folder, word_count):
+def write_word_counts(output_folder, word_counts):
     pass
 
 
@@ -45,5 +44,5 @@ def main():
     lines = read_all_lines(input_folder)
     preprocessed_lines = preprocess_lines(lines)
     words = split_into_words(preprocessed_lines)
-    word_count = count_words(words)
-    write_word_count(output_folder, word_count)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
